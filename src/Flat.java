41bfd598e99ac5e75484f524fc3619499708857e
Vitaly Engel <vitaliy.engel@gmail.com>
2021-10-21 16:02:26 +0300

    Flat set/get template

--- src/Flat.java
+++ src/Flat.java
@@ -4,15 +4,61 @@ public class Flat {
     private double price;
     private double flour;
     private int rooms;
-    private enum remont {no, pred, cosmetic, euro, euroSuper};
-    private enum otoplenie {centr, centrCount, gas, electro};
+    private String repair;
+    private String heat;
+
 
     Flat (double square, double price, int rooms){
         this.square = square;
         this.price = price;
         this.rooms = rooms;
     }
+
     public String getSquare(){
         return this.square + " метров квадратных";
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getFlour() {
+        return flour;
+    }
+
+    public void setFlour(double flour) {
+        this.flour = flour;
+    }
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+
+    public String getRepair() {
+        return repair;
+    }
+
+    public void setRepair(String repair) {
+        this.repair = repair;
+    }
+
+    public String getHeat() {
+        return heat;
+    }
+
+    public void setHeat(String heat) {
+        this.heat = heat;
     }
 }
